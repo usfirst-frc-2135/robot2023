faad77914ab7f948653cd4e03102e48244282407
Jeff Mullins <jeff.l.mullins@gmail.com>
2023-01-12 21:51:24 -0800

    Comp2023 saved the source files with autoformat settings

--- Comp2023/src/main/java/frc/robot/Constants.java
+++ Comp2023/src/main/java/frc/robot/Constants.java
@@ -9,11 +9,14 @@ package frc.robot;
  * constants. This class should not be used for any other purpose. All constants should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
-  public static class OperatorConstants {
+public final class Constants
+{
+  public static class OperatorConstants
+  {
     public static final int kDriverControllerPort = 0;
   }
 }
