aa2e8c918f64d8cf2835181e2859ef747fffa90d
Jeff Mullins <jeff.l.mullins@gmail.com>
2023-02-06 20:03:09 -0800

    Removed example boilerplate and set default gripper motor direction to non-inverted

--- Comp2023/src/main/java/frc/robot/subsystems/Gripper.java
+++ Comp2023/src/main/java/frc/robot/subsystems/Gripper.java
@@ -1,7 +1,6 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+//
+// Gripper subystem - holds/delivers the cubes and cones
+//
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
@@ -9,25 +8,26 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.GRConsts;
-import frc.robot.Constants.Ports;
 import frc.robot.Constants.GRConsts.GRMode;
+import frc.robot.Constants.Ports;
 
+//
+// Gripper subsystem class
+//
 public class Gripper extends SubsystemBase
 {
+  // Member objects
   private final WPI_TalonFX mGripper17 = new WPI_TalonFX(Ports.kCANID_Gripper);
 
-  /** Creates a new ExampleSubsystem. */
   public Gripper( )
   {
     setName("Gripper");
     setSubsystem("Gripper");
 
-    mGripper17.setInverted(true);
+    mGripper17.setInverted(false);
     mGripper17.setSafetyEnabled(false);
-    mGripper17.set(0.0);
 
     initialize( );
-
   }
 
   @Override
@@ -51,7 +51,7 @@ public class Gripper extends SubsystemBase
   public void setGripperSpeed(GRMode mode)
   {
     final String strName;
-    double output = 0.0; //off default
+    double output;
 
     switch (mode)
     {
