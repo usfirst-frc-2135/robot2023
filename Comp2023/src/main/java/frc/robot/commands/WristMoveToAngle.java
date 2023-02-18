2558a39ca395c3077541c38aaca7d53c52132ba4
Jeff Mullins <jmullins16@users.noreply.github.com>
2023-02-17 19:34:19 -0800

    Update m_Angle and Angle for style

--- Comp2023/src/main/java/frc/robot/commands/WristMoveToAngle.java
+++ Comp2023/src/main/java/frc/robot/commands/WristMoveToAngle.java
@@ -13,12 +13,12 @@ import frc.robot.subsystems.Arm;
 public class WristMoveToAngle extends CommandBase
 {
   private final Arm  m_wrist;
-  private WristAngle m_Angle;
+  private WristAngle m_angle;
 
-  public WristMoveToAngle(Arm wrist, WristAngle Angle)
+  public WristMoveToAngle(Arm wrist, WristAngle angle)
   {
     m_wrist = wrist;
-    m_Angle = Angle;
+    m_angle = angle;
 
     setName("ClimberMoveToAngle");
     addRequirements(m_wrist);
@@ -28,7 +28,7 @@ public class WristMoveToAngle extends CommandBase
   @Override
   public void initialize( )
   {
-    m_wrist.moveWristDistanceInit(m_Angle);
+    m_wrist.moveWristDistanceInit(m_angle);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
